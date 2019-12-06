44450c9b472dad9f50e09a6cadab9bef5aeae0c2
RVanDamme <renaudvandamme@gmail.com>
2019-12-06 15:43:05 +0100

    testing conda build

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def main():
           author='Renaud Van Damme',
           author_email='renaud.van.damme@slu.se',
           license='GNU 3.0',
-          scripts=glob('parser.py'),
+          scripts=glob('PANKEGG/parser.py'),
           )
 
 
